7e1b112504edb728b4e1b62b9fc7722edd34cabb
lk <719283438@qq.com>
2016-05-14 09:17:10 +0800

    zyy的第二次提交

--- freechange/Controller/ZyyPageViewController.m
+++ freechange/Controller/ZyyPageViewController.m
@@ -7,62 +7,131 @@
 //
 
 #import "ZyyPageViewController.h"
-#import "ZyyMZGuidePages.h"
-@interface ZyyPageViewController ()
+#import "LGSublimationView.h"
+#import "ZyyTabBarViewController.h"
+
+@interface ZyyPageViewController ()<LGSublimationViewDelegate,UIGestureRecognizerDelegate>
 
 @end
 
 @implementation ZyyPageViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    [self guidePages];
 
-    
-}
-- (void)guidePages
+- (void)viewDidLoad
 {
-    //数据源
-    NSArray *imageArray = @[ @"1.jpg", @"2.jpg", @"3.jpg", @"4.jpg" ];
+    [super viewDidLoad];
     
-    //  初始化方法1
-    ZyyMZGuidePages *mzgpc = [[ZyyMZGuidePages alloc] init];
-    mzgpc.imageDatas = imageArray;
-    __weak typeof(ZyyMZGuidePages) *weakMZ = mzgpc;
-    mzgpc.buttonAction = ^{
-        [UIView animateWithDuration:2.0f
-                         animations:^{
-                             weakMZ.alpha = 0.0;
-                         }
-                         completion:^(BOOL finished) {
-                             [weakMZ removeFromSuperview];
-                         }];
-    };
+//    UIButton * button=[[UIButton alloc]initWithFrame:CGRectMake(100, 100, 200, 30)];
+//    button.backgroundColor=[UIColor orangeColor];
+//    [self.view addSubview:button];
     
-    //  初始化方法2
-    //    MZGuidePagesController *mzgpc = [[MZGuidePagesController alloc]
-    //    initWithImageDatas:imageArray
-    //                                                                            completion:^{
-    //                                                                              NSLog(@"click!");
+}
+- (void)loadView
+{
+    [super loadView];
+    
+    LGSublimationView *lgSublimer = [[LGSublimationView alloc]initWithFrame:self.view.bounds];
+    
+    NSMutableArray * views = [NSMutableArray new];
+    
+    for (int i  = 1; i<=4; i++)
+    {
+        UIImageView *view = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+        
+        view.image = [UIImage imageNamed:[NSString stringWithFormat:@"%i.jpg",i]];
+        
+        view.contentMode = UIViewContentModeScaleAspectFill;
+        
+        [views addObject:view];
+        
+    }
+    
+    
+    lgSublimer.titleLabelTextColor = [UIColor whiteColor];
+    lgSublimer.descriptionLabelTextColor = [UIColor whiteColor];
+    lgSublimer.delegate = self;
+    lgSublimer.titleLabelFont = [UIFont fontWithName:@"HelveticaNeue-Light" size:20];
+    lgSublimer.descriptionLabelFont = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:20];
+    lgSublimer.titleStrings = @[@"布拉德",
+                                @"赫本",
+                                @"阿汤哥",
+                                @"茱莉亚"];
+    lgSublimer.descriptionStrings = @[@"大家好，我是布拉德皮特，欢迎收听小贤有约",
+                                      @"大家好，我是赫本，欢迎收听小贤有约"
+                                      ,@"大家好，我是阿汤哥，欢迎收听小贤有约",
+                                      @"大家好，我是茱莉亚，欢迎收听小贤有约"];
+    
+    
+    lgSublimer.viewsToSublime = views;
+    
+    UIView * shadeView = [[UIView alloc]initWithFrame:lgSublimer.frame];
+    shadeView.backgroundColor = [UIColor blackColor];
+    shadeView.alpha = .5;
+    
+    lgSublimer.inbetweenView = shadeView;
+    
+    [self.view addSubview:lgSublimer];
+    
+    
+}
+
+-(void)lgSublimationView:(LGSublimationView *)view didEndScrollingOnPage:(NSUInteger)page
+{
+    NSLog(@"当前页面:%i",(int)page);
+    
+    if(page == 4)
+    {
+        view.userInteractionEnabled = YES;
+        //点击区分双击和单击
+        UITapGestureRecognizer *doubleTap = [[UITapGestureRecognizer alloc]
+                                             initWithTarget:self
+                                             action:@selector(handleDoubleTap:)];
+        
+        [doubleTap setNumberOfTapsRequired:2];
+        
+        [view addGestureRecognizer:doubleTap];
+        
+        UITapGestureRecognizer* singleTap=[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(singletap:)];
+        [singleTap setNumberOfTouchesRequired:1];
+        [view addGestureRecognizer:singleTap];
+        
+        
+        doubleTap.delegate = self;
+        
+    }
+    
+}
+-(void)singletap:(UIGestureRecognizer*)gestureView
+{
+    NSLog(@"66567");
+}
+
+-(void)handleDoubleTap:(UIGestureRecognizer*)gestureView
+{
+    NSLog(@"在此执行跳转");
+    
+    
+    //    UIStoryboard *storyBoard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
     //
+    //    UIViewController* viewC = [storyBoard instantiateViewControllerWithIdentifier:@"MainViewC"];
     
-    //要在makeKeyAndVisible之后调用才有效
-    [self.view addSubview:mzgpc];
+    ZyyTabBarViewController* first=[[ZyyTabBarViewController alloc]init];
+    
+    //此处可以自定义转场动画，啦啦啦啦啦啦啦
+    
+    [self presentViewController:first animated:YES completion:^{
+        NSLog(@"开始APP");
+    }];
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+//- (void)didReceiveMemoryWarning
+//    {
+//    [super didReceiveMemoryWarning];
+//    // Dispose of any resources that can be recreated.
+//}
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
